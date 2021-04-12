392c9f839801ce662069a262a87802852b476e3b
Matteo Ragazzini <matteo.ragazzini5@studio.unibo.it>
2021-04-12 16:32:43 +0200

    Added implicits in the previous solution

--- src/u06lab/code/Combiner.scala
+++ src/u06lab/code/Combiner.scala
@@ -5,37 +5,10 @@ package u06lab.code
   * in TryFunctions works correctly.
  */
 
-trait Functions {
-  def sum(a: List[Double]): Double
-  def concat(a: Seq[String]): String
-  def max(a: List[Int]): Int // gives Int.MinValue if a is empty
-  def combiner[A](a: Seq[A], combiner: Combiner[A]): A
+trait Combiner[A] {
+  def unit: A
+  def combine(a: A, b: A): A
 }
-
-object FunctionsImpl extends Functions {
-
-  override def sum(a: List[Double]): Double =  a.foldRight(0.0)(_ + _)
-
-
-  override def concat(a: Seq[String]): String = {
-    val st = ""
-    a.toList.foreach(s => st+s)
-    st
-  }
-
-  override def max(a: List[Int]): Int = a match{
-    case Nil => Int.MinValue
-    case _ => a.max
-  }
-
-  def combiner[A](a: Seq[A], combiner: Combiner[A]): A = a match {
-    case Nil => combiner.unit
-    case _ => a.foldRight(combiner.unit)(combiner.combine)
-  }
-}
-
-
-
 /*
   * 2) To apply DRY principle at the best,
   * note the three methods in Functions do something similar.
@@ -49,19 +22,51 @@ object FunctionsImpl extends Functions {
   * When all works, note we completely avoided duplications..
  */
 
-trait Combiner[A] {
-  def unit: A
-  def combine(a: A, b: A): A
+object ImplicitsCombiners {
+
+  implicit object sumCombiner extends Combiner[Double] {
+    override val unit: Double = 0.0
+
+    override def combine(a: Double, b: Double): Double = a + b
+  }
+
+  implicit object concatCombiner extends Combiner[String] {
+    override val unit: String = ""
+
+    override def combine(a: String, b: String): String = a + b
+  }
+
+  implicit object maxCombiner extends Combiner[Int] {
+    override val unit: Int = Int.MinValue
+
+    override def combine(a: Int, b: Int): Int = Math.min(a, b)
+  }
 }
 
-case class sumCombiner(var unit: Double = 0.0) extends  Combiner[Double] {
-  override def combine(a: Double, b: Double): Double = a + b
+trait Functions {
+  def sum(a: List[Double]): Double
+  def concat(a: Seq[String]): String
+  def max(a: List[Int]): Int // gives Int.MinValue if a is empty
+  def combine[A:Combiner](a: Seq[A]): A
 }
 
-case class concatCombiner(var unit: String = "") extends Combiner[String] {
-  override def combine(a: String, b: String): String = a + b
+object FunctionsImpl extends Functions {
+  import ImplicitsCombiners._
+
+  override def sum(a: List[Double]): Double = combine(a)
+
+
+  override def concat(a: Seq[String]): String = combine(a)
+
+  override def max(a: List[Int]): Int = combine(a)
+
+  def combine[A:Combiner](a: Seq[A]): A = a match {
+    case Nil => implicitly[Combiner[A]].unit
+    case _ => a.foldRight(implicitly[Combiner[A]].unit)(implicitly[Combiner[A]].combine)
+  }
 }
 
-case class maxCombiner(var unit: Int = Int.MinValue) extends Combiner[Int] {
-  override def combine(a: Int, b: Int): Int = Math.max(a,b)
-}
+
+
+
+
--- test/u06lab/code/Combiner.scala
+++ test/u06lab/code/Combiner.scala
@@ -13,13 +13,6 @@ class TryFunctions {
     println("", f.concat(Seq()))
     println(3, f.max(List(-10, 3, -5, 0)))
     println(Integer.MIN_VALUE, f.max(List()))
-
-    assertEquals(60.1, f.combiner(List(10.0, 20.0, 30.1), new sumCombiner), 0.001) // 60.1
-    println(0.0, f.combiner(List(), new sumCombiner)) // 0.0
-    println("abc", f.combiner(Seq("a", "b", "c"), new concatCombiner))
-    println("", f.combiner(Seq(), new concatCombiner ))
-    println(3, f.combiner(List(-10, 3, -5, 0), new maxCombiner))
-    println(Integer.MIN_VALUE, f.combiner(List(), new maxCombiner))
   }
 }
 
